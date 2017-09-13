1b738ddf3f2ac86e2acb637b98c7146d0e95e8fa
DanielKnoll <knoll.dani@gmail.com>
2017-09-13 19:51:58 +0200

    Renamed misspelled variables, all_shools query done except "No data part".

--- queries.py
+++ queries.py
@@ -3,9 +3,18 @@ from database_common import connection_handler
 
 @connection_handler
 def mentors_and_schools():
-    quary = """SELECT mentors.first_name, mentors.last_name, schools.name, schools.country
+    query = """SELECT mentors.first_name, mentors.last_name, schools.name, schools.country
                FROM mentors JOIN schools
                ON (mentors.city = schools.city)
                ORDER BY mentors.id;"""
+    return query
+
+
+@connection_handler
+def all_schools():
+    query = """SELECT mentors.first_name, mentors.last_name, schools.name, schools.country
+               FROM mentors RIGHT JOIN schools
+               ON (mentors.city = schools.city)
+               ORDER BY mentors.id;"""
     values = ""
-    return quary, values
+    return query
