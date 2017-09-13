c96786d6fcc51f99e0be31f02cfe2a4ba62df7a8
DanielKnoll <knoll.dani@gmail.com>
2017-09-13 20:31:24 +0200

    Null value replaced with 'No data' string.

--- queries.py
+++ queries.py
@@ -12,9 +12,10 @@ def mentors_and_schools():
 
 @connection_handler
 def all_schools():
-    query = """SELECT mentors.first_name, mentors.last_name, schools.name, schools.country
+    query = """SELECT COALESCE(mentors.first_name, 'No data') AS first_name,
+                      COALESCE(mentors.last_name, 'No data') AS last_name,
+                      schools.name, schools.country
                FROM mentors RIGHT JOIN schools
                ON (mentors.city = schools.city)
                ORDER BY mentors.id;"""
-    values = ""
     return query
